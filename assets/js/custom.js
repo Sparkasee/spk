8d87231e91b155b809964a66a98930c70e617921
Sparkasee <119666690+Sparkasee@users.noreply.github.com>
2022-12-02 16:00:58 +0530

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.shw_hidden').click(function(){
+        $('.right__left__side').toggleClass('d-none');
+    });
+});
+
+
+
+
+
+$(document).ready(function(){
+    $('.hamburgr').click(function(){
+        $('.header__section').toggleClass('d-none');
+    });
+});
+
+
+
+// $(document).ready(function(){
+//     $('.click_sw_error_sparkse').click(function(){
+
+//     $('.eror__div_shw').removeClass('d-none');
+//     $('.form__heading').addClass('d-none');
+
+//     });
+//     });
